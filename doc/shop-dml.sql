35568885bd4a374f0b31f5a896177d9dd06dc859
zhoujunlin <zhoujunlin.work@outlook.com>
2024-06-06 23:03:17 +0800

    product

---
+++ doc/shop-dml.sql
@@ -0,0 +1,7 @@
+USE `product`;
+
+insert into shop_product (p_name, p_price, stock)
+values ('小米', 200000, 5000),
+       ('OPPO', 300000, 5000),
+       ('华为', 400000, 5000),
+       ('苹果', 500000, 5000);
---
+++ product-srv/product-server/src/main/java/io/github/zhoujunlin94/server/product/service/impl/ProductServiceImpl.java
@@ -0,0 +1,25 @@
+package io.github.zhoujunlin94.server.product.service.impl;
+
+import cn.hutool.core.bean.BeanUtil;
+import io.github.zhoujunlin94.api.product.ProductDTO;
+import io.github.zhoujunlin94.server.product.repository.handler.ProductHandler;
+import io.github.zhoujunlin94.server.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author zhoujunlin
+ * @date 2024/6/6 22:47
+ */
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductHandler productHandler;
+
+    @Override
+    public ProductDTO findById(Integer productId) {
+        return BeanUtil.toBean(productHandler.selectByPrimaryKey(productId), ProductDTO.class);
+    }
+
+}
