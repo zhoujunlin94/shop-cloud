76d2fcb7ec31cef1fec91960ff42cb43a1bc4647
zhoujunlin <zhoujunlin.work@outlook.com>
2024-06-09 23:05:14 +0800

    gateway

---
+++ api-gateway/src/main/java/io/github/zhoujunlin94/gateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package io.github.zhoujunlin94.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhoujunlin
+ * @date 2024/6/9 22:47
+ */
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
