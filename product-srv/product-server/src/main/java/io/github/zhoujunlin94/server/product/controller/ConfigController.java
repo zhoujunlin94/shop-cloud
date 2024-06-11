c21b55efcfd78107c19b59627a3a2f33662822d7
zhoujunlin <zhoujunlin.work@outlook.com>
2024-06-11 22:48:39 +0800

    config

---
+++ product-srv/product-server/src/main/java/io/github/zhoujunlin94/server/product/controller/ConfigController.java
@@ -0,0 +1,26 @@
+package io.github.zhoujunlin94.server.product.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhoujunlin
+ * @date 2024/6/11 22:42
+ */
+@RefreshScope
+@RestController
+@RequestMapping("/api/v1/config")
+public class ConfigController {
+
+    @Value("${app.config.name:}")
+    private String appConfigName;
+
+    @GetMapping("echoAppConfigName")
+    public String echoAppConfigName() {
+        return appConfigName;
+    }
+
+}
