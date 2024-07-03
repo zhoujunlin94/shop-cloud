cf66fd5f0be2dd93849b40144fc8570fefe43d05
zhoujunlin <zhoujunlin.work@outlook.com>
2024-07-03 22:54:26 +0800

    RocketMQ

--- sso-server/src/main/java/io/github/zhoujunlin94/server/sso/repository/mq/service/impl/RocketMQService.java
+++ sso-server/src/main/java/io/github/zhoujunlin94/server/sso/repository/mq/service/impl/RocketMQService.java
@@ -1,13 +1,17 @@
 package io.github.zhoujunlin94.server.sso.repository.mq.service.impl;
 
-import com.alibaba.fastjson.JSONObject;
+import io.github.zhoujunlin94.meet.common.util.RequestIdUtil;
 import io.github.zhoujunlin94.server.sso.repository.mq.service.MQService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.client.producer.SendStatus;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Service;
+
+import java.util.function.BiConsumer;
 
 /**
  * @author zhoujunlin
@@ -24,8 +28,29 @@ public class RocketMQService implements MQService {
     public boolean syncSend(String topic, Object payload) {
         log.warn("syncSend RocketMQ, topic:{}, payload:{}", topic, payload);
         SendResult sendResult = rocketMQTemplate.syncSend(topic, payload);
-        log.warn("syncSend RocketMQ SendResult:{}", JSONObject.toJSONString(sendResult));
+        log.warn("syncSend RocketMQ SendResult:{}", sendResult);
         return sendResult.getSendStatus() == SendStatus.SEND_OK;
+    }
+
+    public void asyncSend(String topic, Object payload, BiConsumer<String, Object> successConsumer, BiConsumer<String, Object> exceptionConsumer) {
+        log.warn("asyncSend RocketMQ, topic:{}, payload:{}", topic, payload);
+        String requestId = MDC.get(RequestIdUtil.REQUEST_ID);
+        rocketMQTemplate.asyncSend(topic, payload, new SendCallback() {
+            @Override
+            public void onSuccess(SendResult sendResult) {
+                MDC.put(RequestIdUtil.REQUEST_ID, requestId);
+                log.warn("[asyncSend][消息体:{} 发送成功, 结果为:{}]", payload, sendResult);
+                successConsumer.accept(topic, payload);
+            }
+
+            @Override
+            public void onException(Throwable throwable) {
+                MDC.put(RequestIdUtil.REQUEST_ID, requestId);
+                log.warn("[asyncSend][消息体:{} 发送异常]", payload, throwable);
+                exceptionConsumer.accept(topic, payload);
+            }
+
+        });
     }
 
     @Override
