c5eed98a791b4f74b3acb45302f512881a5b16cd
zhoujunlin <zhoujunlin@gaodun.com>
2024-07-10 09:45:36 +0800

    feign

--- api-gateway/src/main/java/io/github/zhoujunlin94/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/io/github/zhoujunlin94/gateway/ApiGatewayApplication.java
@@ -2,11 +2,13 @@ package io.github.zhoujunlin94.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author zhoujunlin
  * @date 2024/6/9 22:47
  */
+@EnableFeignClients
 @SpringBootApplication
 public class ApiGatewayApplication {
 
