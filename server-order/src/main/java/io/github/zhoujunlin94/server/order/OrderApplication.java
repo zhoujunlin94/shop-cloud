7164a16254e074956211c22344b82149149249a0
zhoujunlin <zhoujunlin@gaodun.com>
2024-07-12 16:51:42 +0800

    seata测试

--- server-order/src/main/java/io/github/zhoujunlin94/server/order/OrderApplication.java
+++ server-order/src/main/java/io/github/zhoujunlin94/server/order/OrderApplication.java
@@ -1,30 +1,23 @@
 package io.github.zhoujunlin94.server.order;
 
-import io.seata.rm.datasource.DataSourceProxy;
+import io.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-import javax.sql.DataSource;
 
 /**
  * @author zhoujunlin
  * @date 2024-06-07-10:15
  */
 @SpringBootApplication
-@EnableFeignClients(basePackages = {"io.github.zhoujunlin94.server.order.repository.feign.client"})
+@EnableAutoDataSourceProxy
 @EnableAspectJAutoProxy(exposeProxy = true)
+@EnableFeignClients(basePackages = {"io.github.zhoujunlin94.server.order.repository.feign.client"})
 public class OrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
-    }
-
-    @Bean
-    public DataSource dataSource(DataSource accountDataSource) {
-        return new DataSourceProxy(accountDataSource);
     }
 
 }
--- server-product/src/main/java/io/github/zhoujunlin94/server/product/ProductApplication.java
+++ server-product/src/main/java/io/github/zhoujunlin94/server/product/ProductApplication.java
@@ -1,28 +1,21 @@
 package io.github.zhoujunlin94.server.product;
 
-import io.seata.rm.datasource.DataSourceProxy;
+import io.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-import javax.sql.DataSource;
 
 /**
  * @author zhoujunlin
  * @date 2024/6/6 22:31
  */
 @SpringBootApplication
+@EnableAutoDataSourceProxy
 @EnableAspectJAutoProxy(exposeProxy = true, proxyTargetClass = true)
 public class ProductApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
-    }
-
-    @Bean
-    public DataSource dataSource(DataSource accountDataSource) {
-        return new DataSourceProxy(accountDataSource);
     }
 
 }
